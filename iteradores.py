38275c42295010e9fb2c55123be7c4c2aa3955e5
Camilo Diaz <camilo20263@gmail.com>
2025-12-15 10:20:22 -0500

    iterador

--- iteradores.py
+++ iteradores.py
@@ -1,1 +1,15 @@
-print('hola')
+# Crear una lista
+lista = [1, 2, 3, 4]
+print('Lista original:', lista) # Imprime: [1, 2, 3, 4]
+# Obtener el iterador de la lista
+iterador = iter(lista)
+
+# Usar el iterador para obtener elementos
+print(next(iterador))  # Imprime: 1
+print(next(iterador))  # Imprime: 2
+print(next(iterador))  # Imprime: 3
+print(next(iterador))  # Imprime: 4
+
+
+# Intentar obtener otro elemento después de finalizar la iteración
+print(next(iterador))  # Esto generará una excepción StopIteration
