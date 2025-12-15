fda5a95e83bdc556b9021e15e21f9d2df7e8ae5d
Camilo Diaz <camilo20263@gmail.com>
2025-12-15 10:28:07 -0500

    iterador para números impares hasta 10

--- iteradores.py
+++ iteradores.py
@@ -27,3 +27,14 @@ print(next(var_text))  # Imprime: a
 #--- Uso de un bucle for para iterar sobre una cadena
 for char in text:
     print(char) # Imprime cada carácter en una línea separada
+
+
+
+
+# Crear un iterador para números impares hasta 10
+limite = 10
+iterador_impares = iter(range(1, limite + 1, 2))
+
+# Iterar a través de los números impares
+for numero in iterador_impares:
+    print(numero)
