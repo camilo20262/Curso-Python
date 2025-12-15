f56100566e93d2e22248809ebc10109f981476fa
Camilo Diaz <camilo20263@gmail.com>
2025-12-15 10:22:56 -0500

    iterar sobre una cadena

--- iteradores.py
+++ iteradores.py
@@ -12,4 +12,18 @@ print(next(iterador))  # Imprime: 4
 
 
 # Intentar obtener otro elemento después de finalizar la iteración
-print(next(iterador))  # Esto generará una excepción StopIteration
+##print(next(iterador))  # Esto generará una excepción StopIteration
+
+
+#--- 
+text="hola"
+var_text=iter(text)
+print(next(var_text))  # Imprime: h
+print(next(var_text))  # Imprime: o    
+print(next(var_text))  # Imprime: l
+print(next(var_text))  # Imprime: a
+##print(next(var_text))  # Esto generará una excepción StopIteration
+
+#--- Uso de un bucle for para iterar sobre una cadena
+for char in text:
+    print(char) # Imprime cada carácter en una línea separada
